52704d3c21db027959dc9418b631d81101745790
is2win <is2win@yandex.ru>
2023-04-15 19:30:55 +0300

    добавление дружлюбных ссылок на пост

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,14 +8,18 @@ def post_list(request: HttpRequest):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request: HttpRequest, id: int):
+def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str) -> HttpResponse:
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404('No Post Found')
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED)
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
 
     return render(request, 'blog/post/detail.html', {'post': post})
