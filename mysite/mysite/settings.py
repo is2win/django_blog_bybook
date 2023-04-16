f84c68ba4804bcbe72f6d962dd193295923dd7d5
is2win <is2win@yandex.ru>
2023-04-16 11:45:15 +0300

    Добавлена опция - делиться поставми через e-mail

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Конфигурация сервера электронной почты - буду использовать принт в консоль
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
