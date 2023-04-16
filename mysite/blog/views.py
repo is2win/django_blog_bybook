503d746c74767fc0c2e0ad283fee622338094c5a
is2win <is2win@yandex.ru>
2023-04-16 10:32:08 +0300

    Реализация списка постов через класс ListView

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,20 +2,30 @@ from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+# def post_list(request: HttpRequest):
+#     all_posts = Post.published.all()
+#     #  добавляем пагинатор
+#     paginator = Paginator(all_posts, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_list(request: HttpRequest):
-    all_posts = Post.published.all()
-    #  добавляем пагинатор
-    paginator = Paginator(all_posts, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str) -> HttpResponse:
